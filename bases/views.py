a0a1c4952478eb78826af901f3c93e66bd6c78d2
Miguel Angel Vega Alonso <mivegaal@everis.com>
2020-11-06 10:50:29 -0500

    Se ajusta template de login

--- bases/views.py
+++ bases/views.py
@@ -5,4 +5,4 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
-    login_url = '/login'
+    login_url = 'bases:login'
